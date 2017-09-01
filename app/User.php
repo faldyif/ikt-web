b43aa6166e14aff7e100e363c0ec8c3473c2c6db
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2017-09-01 23:05:09 +0700

    Fix for pragmarx tracker

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return bool
+     */
+    public function getIsAdminAttribute()
+    {
+        return true;
+    }
 }
--- config/database.php
+++ config/database.php
@@ -89,7 +89,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,    // to avoid problems on some MySQL installs
             'engine' => null,
         ],
 
