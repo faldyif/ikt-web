c2602196aa75337471c91b350482e8b1efecfb1e
farhanmuhammad <najibfarhan3@gmail.com>
2017-10-04 06:42:01 +0700

    pict corporate culture added

--- resources/views/company-culture-1.blade.php
+++ resources/views/company-culture-1.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <!--HERO-->
     <section id="heroTitle">
-        <div class="heroTitle" style="background-image: url('./img/2.jpg');">
+        <div class="heroTitle" style="background-image: url('{{ url('img/2.jpg') }}');">
             <div class="overlay"></div>
             <section class="container-fluid pd-30">
                 <h2 class="roboBold wh alignCenter" data-wow-delay="0.4s">{{ trans('company-culture.head-title') }}</h2>
@@ -19,10 +19,6 @@
         <div class="container-fluid wow fadeInUpBig">
           <section class="row pd-bt-30 mg-bt-20">
             <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
-            <div class="col-md-offset-3">
-                <h3 class="mg-b-30 roboBold">Tagline</h3>
-                <h4>Dengan CINTA, satukan langkah mewujudkan kerja nyata IPC Car Terminal berkelas dunia.</h4>
-              </div>
               <section class="row be-block-768">
                 <div class="col-md-3">
                   <ul class="nav nav-pills nav-stacked">
@@ -52,39 +48,68 @@
                 <div class="col-md-9">
                   <div class="tab-content">
                     <div id="home" class="tab-pane fade in active">
-                    <h3 class="mg-b-30 roboBold">Nilai-Nilai Perusahaan</h3>
-                    <p>
-                    <h4>1. Customer Cetric<br><small>Meet Customer Expectation</small></h4>
-                    Secara proaktif mencari tahu serta memahami kebutuhan pelanggan untuk memberikan solusi-solusi yang inovatif.<br>
-                    Membangun hubungan jangka panjang yang baik dengan para pelanggan.<br>
-                    Secara konsisten memberikan pelayanan terbaik dan berkualitas untuk membantu para pelanggan tumbuh dan berkembang.<br>
-                    </p>
-                    <p>
-                    <h4>2. Integrity<br><small>Walk the Talk</small></h4>
-                    Menumbuhkan rasa percaya dengan mengatakan apa yang kita rasakan serta melakukan apa yang kita ucapkan.<br>
-                    Menunjukkan sikap profesional dan jujur dalam berinterasi dengan pihak internal maupun eksternal.<br>
-                    Berperilaku disiplin dan patuh terhadap kode etik bisnis di dalam melakukan pelkerjaan kita sehari-hari.<br>
-                    </p>
-                    <p>
-                    <h4>3. Nationalism<br><small>National Pride</small></h4>
-                    Menumbuhkan semangat dan ikut berperan mensukseskan program pemerintah dalam pembangunan nasional.<br>
-                    Menumbuhkan rasa bangga dan semangat masionalisme dalam berkarya.<br>
-                    Terus berkembang dan mampu bersaing dengan perusahaan pengelola pelabuhan kelas dunia.<br>
-                    </p>
-                    <p>
-                    <h4>4. Team Work<br><small>Together We Can</small></h4>
-                    Berkolaborasi dalam tim untuk mendapatkan hasil yang terbaik<br>
-                    Bekerja bersama-sama menghasilkan ide-ide implementatif untuk solusi kebutuhan pengguna jasa pelabuhan.<br>
-                    Semangat kebersamaan dan menghargai orang lain.<br>
-                    </p>
-                    <p>
-                    <h4>5. Action<br><small>Make It Happy</small></h4>
-                    Berani bermimpi dan berusaha mewujudkannya.<br>
-                    Proaktif untuk mencari cara dalam mewujudkan visi perusahaan.<br>
-                    Melakukan terobosan-terobosan dan langkat nyata dalam mendorong perkembangan perusahaan.<br>
-                    </p>
+                      <h3 class="mg-b-30 roboBold">Tagline</h3>
+                      <h4>Dengan CINTA, satukan langkah mewujudkan kerja nyata IPC Car Terminal berkelas dunia.</h4>
+                      <h3 class="mg-b-30 roboBold">Nilai-Nilai Perusahaan</h3>
+                      <div class="col-md-4">
+                        <img src="{{ url('img/1_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 10px 0">
+                      </div>
+                      <div class="col-md-8">
+                        <p>
+                          <h4>1. Customer Cetric<br><small>Meet Customer Expectation</small></h4>
+                          Secara proaktif mencari tahu serta memahami kebutuhan pelanggan untuk memberikan solusi-solusi yang inovatif.<br>
+                          Membangun hubungan jangka panjang yang baik dengan para pelanggan.<br>
+                          Secara konsisten memberikan pelayanan terbaik dan berkualitas untuk membantu para pelanggan tumbuh dan berkembang.<br>
+                        </p>
+                      </div>
+                      <div class="col-md-4">
+                        <img src="{{ url('img/2_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 20px 0">
+                      </div>
+                      <div class="col-md-8">
+                        <p>
+                        <h4>2. Integrity<br><small>Walk the Talk</small></h4>
+                        Menumbuhkan rasa percaya dengan mengatakan apa yang kita rasakan serta melakukan apa yang kita ucapkan.<br>
+                        Menunjukkan sikap profesional dan jujur dalam berinterasi dengan pihak internal maupun eksternal.<br>
+                        Berperilaku disiplin dan patuh terhadap kode etik bisnis di dalam melakukan pelkerjaan kita sehari-hari.<br>
+                        </p>
+                      </div>
+                      <div class="col-md-4">
+                        <img src="{{ url('img/3_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 10px 0">
+                      </div>
+                      <div class="col-md-8">
+                        <p>
+                        <h4>3. Nationalism<br><small>National Pride</small></h4>
+                        Menumbuhkan semangat dan ikut berperan mensukseskan program pemerintah dalam pembangunan nasional.<br>
+                        Menumbuhkan rasa bangga dan semangat masionalisme dalam berkarya.<br>
+                        Terus berkembang dan mampu bersaing dengan perusahaan pengelola pelabuhan kelas dunia.<br>
+                        </p>
+                      </div>
+                      <div class="col-md-4">
+                        <img src="{{ url('img/4_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 5px 0">
+                      </div>
+                      <div class="col-md-8">
+                        <p>
+                        <h4>4. Team Work<br><small>Together We Can</small></h4>
+                        Berkolaborasi dalam tim untuk mendapatkan hasil yang terbaik<br>
+                        Bekerja bersama-sama menghasilkan ide-ide implementatif untuk solusi kebutuhan pengguna jasa pelabuhan.<br>
+                        Semangat kebersamaan dan menghargai orang lain.<br>
+                        </p>
+                      </div>
+                      <div class="col-md-4">
+                        <img src="{{ url('img/5_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 10px 0">
+                      </div>
+                      <div class="col-md-8">
+                        <p>
+                        <h4>5. Action<br><small>Make It Happy</small></h4>
+                        Berani bermimpi dan berusaha mewujudkannya.<br>
+                        Proaktif untuk mencari cara dalam mewujudkan visi perusahaan.<br>
+                        Melakukan terobosan-terobosan dan langkat nyata dalam mendorong perkembangan perusahaan.<br>
+                        </p>
+                      </div>
                     </div>
                     <div id="menu1" class="tab-pane fade">
+                    <h3 class="mg-b-30 roboBold">Tagline</h3>
+                    <h4>Dengan CINTA, satukan langkah mewujudkan kerja nyata IPC Car Terminal berkelas dunia.</h4>
                     <h3 class="mg-b-30 roboBold">Karakter Perusahaan</h3>
                     <p>
                     <h4>Less Bureaucratic<br><small>Decision Making Process</small></h4>
@@ -104,6 +129,8 @@
                     </p>
                     </div>
                     <div id="menu2" class="tab-pane fade">
+                    <h3 class="mg-b-30 roboBold">Tagline</h3>
+                    <h4>Dengan CINTA, satukan langkah mewujudkan kerja nyata IPC Car Terminal berkelas dunia.</h4>
                     <h3 class="mg-b-30 roboBold">Landasan/Dasar dalam Melaksanaan Tugas dan Kewajiban Perusahaan</h3>
                     <p>
                     <h4>Going Concern<br></h4>
@@ -137,7 +164,7 @@
         <section class="row">
             <div class="col-md-3 col-sm-3">
                 <figure class="logoFooter">
-                    <img src="./img/ikt-logo.png">
+                    <img src="{{url('img/ikt-logo.png')}}">
                 </figure>
                 <p class="small">Vix eu etiam mediocrem. Qui debet essent omnium ut. Ne qui recusabo temporibus, in eum singulis posidonium. Nec reque saepe ea, esse veniam definitionem est ei.</p>
             </div>
--- resources/views/csr.blade.php
+++ resources/views/csr.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <!--HERO-->
     <section id="heroTitle">
-        <div class="heroTitle" style="background-image: url('./img/2.jpg');">
+        <div class="heroTitle" style="background-image: url('{{ url('img/2.jpg') }}');">
             <div class="overlay"></div>
             <section class="container-fluid pd-30">
                 <h2 class="roboBold wh alignCenter" data-wow-delay="0.4s">CSR IPC Car Terminal - Rangkul Warga</h2>
