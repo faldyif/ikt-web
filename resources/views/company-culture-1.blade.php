b94c17aa801b3c85b4be9df98e261943f34ff815
farhanmuhammad <najibfarhan3@gmail.com>
2017-10-04 22:40:00 +0700

    pic corporate culture and revision #2

--- resources/views/company-culture-1.blade.php
+++ resources/views/company-culture-1.blade.php
@@ -23,13 +23,6 @@
                 <div class="col-md-3">
                   <ul class="nav nav-pills nav-stacked">
                       <li class="active">
-                          <a data-toggle="pill" href="#tagline">
-                              <div class="be-flex">
-                                  <i class="fa fa-th mg-r-15 mg-t-5"></i> <span>Tagline</span>
-                              </div>
-                          </a>
-                      </li>
-                      <li class="">
                           <a data-toggle="pill" href="#home">
                               <div class="be-flex">
                                   <i class="fa fa-th mg-r-15 mg-t-5"></i> <span>Karakter Perusahaan</span>
@@ -58,82 +51,120 @@
                       <h3 class="mg-b-30 roboBold">Tagline</h3>
                       <h4>Dengan CINTA, satukan langkah mewujudkan kerja nyata IPC Car Terminal berkelas dunia.</h4>
                       <h3 class="mg-b-30 roboBold">Nilai-Nilai Perusahaan</h3>
-                      <div class="col-md-4">
-                        <img src="{{ url('img/1_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 10px 0">
+                      <div class="col-md-12">
+                        <div class="col-md-2">
+                          <img src="{{ url('img/1_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 30px 0">
+                        </div>
+                        <div class="col-md-10">
+                          <p>
+                            <h4>1. Customer Cetric<br><small>Meet Customer Expectation</small></h4>
+                            Secara proaktif mencari tahu serta memahami kebutuhan pelanggan untuk memberikan solusi-solusi yang inovatif.<br>
+                            Membangun hubungan jangka panjang yang baik dengan para pelanggan.<br>
+                            Secara konsisten memberikan pelayanan terbaik dan berkualitas untuk membantu para pelanggan tumbuh dan berkembang.<br>
+                          </p>
+                        </div>
                       </div>
-                      <div class="col-md-8">
-                        <p>
-                          <h4>1. Customer Cetric<br><small>Meet Customer Expectation</small></h4>
-                          Secara proaktif mencari tahu serta memahami kebutuhan pelanggan untuk memberikan solusi-solusi yang inovatif.<br>
-                          Membangun hubungan jangka panjang yang baik dengan para pelanggan.<br>
-                          Secara konsisten memberikan pelayanan terbaik dan berkualitas untuk membantu para pelanggan tumbuh dan berkembang.<br>
-                        </p>
+                      <div class="col-md-12">
+                        <div class="col-md-2">
+                          <img src="{{ url('img/2_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 15px 0">
+                        </div>
+                        <div class="col-md-10">
+                          <p>
+                          <h4>2. Integrity<br><small>Walk the Talk</small></h4>
+                          Menumbuhkan rasa percaya dengan mengatakan apa yang kita rasakan serta melakukan apa yang kita ucapkan.<br>
+                          Menunjukkan sikap profesional dan jujur dalam berinterasi dengan pihak internal maupun eksternal.<br>
+                          Berperilaku disiplin dan patuh terhadap kode etik bisnis di dalam melakukan pelkerjaan kita sehari-hari.<br>
+                          </p>
+                        </div>
                       </div>
-                      <div class="col-md-4">
-                        <img src="{{ url('img/2_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 20px 0">
+                      <div class="col-md-12">
+                        <div class="col-md-2">
+                          <img src="{{ url('img/3_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 15px 0">
+                        </div>
+                        <div class="col-md-10">
+                          <p>
+                          <h4>3. Nationalism<br><small>National Pride</small></h4>
+                          Menumbuhkan semangat dan ikut berperan mensukseskan program pemerintah dalam pembangunan nasional.<br>
+                          Menumbuhkan rasa bangga dan semangat masionalisme dalam berkarya.<br>
+                          Terus berkembang dan mampu bersaing dengan perusahaan pengelola pelabuhan kelas dunia.<br>
+                          </p>
+                        </div>
                       </div>
-                      <div class="col-md-8">
-                        <p>
-                        <h4>2. Integrity<br><small>Walk the Talk</small></h4>
-                        Menumbuhkan rasa percaya dengan mengatakan apa yang kita rasakan serta melakukan apa yang kita ucapkan.<br>
-                        Menunjukkan sikap profesional dan jujur dalam berinterasi dengan pihak internal maupun eksternal.<br>
-                        Berperilaku disiplin dan patuh terhadap kode etik bisnis di dalam melakukan pelkerjaan kita sehari-hari.<br>
-                        </p>
+                      <div class="col-md-12">
+                        <div class="col-md-2">
+                          <img src="{{ url('img/4_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 15px 0">
+                        </div>
+                        <div class="col-md-10">
+                          <p>
+                          <h4>4. Team Work<br><small>Together We Can</small></h4>
+                          Berkolaborasi dalam tim untuk mendapatkan hasil yang terbaik<br>
+                          Bekerja bersama-sama menghasilkan ide-ide implementatif untuk solusi kebutuhan pengguna jasa pelabuhan.<br>
+                          Semangat kebersamaan dan menghargai orang lain.<br>
+                          </p>
+                        </div>
                       </div>
-                      <div class="col-md-4">
-                        <img src="{{ url('img/3_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 10px 0">
-                      </div>
-                      <div class="col-md-8">
-                        <p>
-                        <h4>3. Nationalism<br><small>National Pride</small></h4>
-                        Menumbuhkan semangat dan ikut berperan mensukseskan program pemerintah dalam pembangunan nasional.<br>
-                        Menumbuhkan rasa bangga dan semangat masionalisme dalam berkarya.<br>
-                        Terus berkembang dan mampu bersaing dengan perusahaan pengelola pelabuhan kelas dunia.<br>
-                        </p>
-                      </div>
-                      <div class="col-md-4">
-                        <img src="{{ url('img/4_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 5px 0">
-                      </div>
-                      <div class="col-md-8">
-                        <p>
-                        <h4>4. Team Work<br><small>Together We Can</small></h4>
-                        Berkolaborasi dalam tim untuk mendapatkan hasil yang terbaik<br>
-                        Bekerja bersama-sama menghasilkan ide-ide implementatif untuk solusi kebutuhan pengguna jasa pelabuhan.<br>
-                        Semangat kebersamaan dan menghargai orang lain.<br>
-                        </p>
-                      </div>
-                      <div class="col-md-4">
-                        <img src="{{ url('img/5_corporate_culture.png') }}" class="img-responsive" style="width: 130px; height: auto; margin: 10px 0">
-                      </div>
-                      <div class="col-md-8">
-                        <p>
-                        <h4>5. Action<br><small>Make It Happy</small></h4>
-                        Berani bermimpi dan berusaha mewujudkannya.<br>
-                        Proaktif untuk mencari cara dalam mewujudkan visi perusahaan.<br>
-                        Melakukan terobosan-terobosan dan langkat nyata dalam mendorong perkembangan perusahaan.<br>
-                        </p>
+                      <div class="col-md-12">
+                        <div class="col-md-2">
+                          <img src="{{ url('img/5_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 10px 0">
+                        </div>
+                        <div class="col-md-10">
+                          <p>
+                          <h4>5. Action<br><small>Make It Happy</small></h4>
+                          Berani bermimpi dan berusaha mewujudkannya.<br>
+                          Proaktif untuk mencari cara dalam mewujudkan visi perusahaan.<br>
+                          Melakukan terobosan-terobosan dan langkat nyata dalam mendorong perkembangan perusahaan.<br>
+                          </p>
+                        </div>
                       </div>
                     </div>
                     <div id="menu1" class="tab-pane fade">
                     <h3 class="mg-b-30 roboBold">Tagline</h3>
                     <h4>Dengan CINTA, satukan langkah mewujudkan kerja nyata IPC Car Terminal berkelas dunia.</h4>
                     <h3 class="mg-b-30 roboBold">Karakter Perusahaan</h3>
-                    <p>
-                    <h4>Less Bureaucratic<br><small>Decision Making Process</small></h4>
-                    PT. Indonesia Kendaraan Terminal memiliki karakter cepat, tepat dan akurat dalam proses pengambilan keputusan.
-                    </p>
-                    <p>
-                    <h4>Less Feudalism<br><small>Professional Intimacy</small></h4>
-                    PT. Indonesia Kendaraan Terminal memiliki karakter yakni hubungan komunikasi yang dibangun bedasarkan profesionalisme kerja.
-                    </p>
-                    <p>
-                    <h4>More Modern<br><small>Werld Class Company, Technology Based &amp Customer Centric</small></h4>
-                    PT. Indonesia Kendaraan Terminal memiliki karakter perusahaan berkelas dunia, yang berbasis IT dan fokus terhadap kepuasan pelanggan.
-                    </p>
-                    <p>
-                    <h4>More Friendly<br><small>World Class Service</small></h4>
-                    PT. Indonesia Kendaraan Terminal memiliki karakter pelayanan optimal, berkelas dunia dan memiliki hubungan yang dekat dengan pelanggan.
-                    </p>
+                    <div class="col-md-12">
+                      <div class="col-md-2">
+                        <img src="{{ url('img/6_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 5px 0">
+                      </div>
+                      <div class="col-md-10">
+                      <p>
+                      <h4>Less Bureaucratic<br><small>Decision Making Process</small></h4>
+                      PT. Indonesia Kendaraan Terminal memiliki karakter cepat, tepat dan akurat dalam proses pengambilan keputusan.
+                      </p>
+                      </div>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="col-md-2">
+                          <img src="{{ url('img/7_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 5px 0">
+                      </div>
+                      <div class="col-md-10">
+                      <p>
+                      <h4>Less Feudalism<br><small>Professional Intimacy</small></h4>
+                      PT. Indonesia Kendaraan Terminal memiliki karakter yakni hubungan komunikasi yang dibangun bedasarkan profesionalisme kerja.
+                      </p>
+                      </div>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="col-md-2">
+                          <img src="{{ url('img/8_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 5px 0">
+                      </div>
+                      <div class="col-md-10">
+                      <p>
+                      <h4>More Modern<br><small>Werld Class Company, Technology Based &amp Customer Centric</small></h4>
+                      PT. Indonesia Kendaraan Terminal memiliki karakter perusahaan berkelas dunia, yang berbasis IT dan fokus terhadap kepuasan pelanggan.
+                      </p>
+                      </div>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="col-md-2">
+                          <img src="{{ url('img/9_corporate_culture.png') }}" class="img-responsive" style="width: 100px; height: auto; margin: 5px 0">
+                      </div>
+                      <div class="col-md-10">
+                      <p>
+                      <h4>More Friendly<br><small>World Class Service</small></h4>
+                      PT. Indonesia Kendaraan Terminal memiliki karakter pelayanan optimal, berkelas dunia dan memiliki hubungan yang dekat dengan pelanggan.
+                      </p>
+                      </div>
+                    </div>
                     </div>
                     <div id="menu2" class="tab-pane fade">
                     <h3 class="mg-b-30 roboBold">Tagline</h3>
