fdf62d9e30bc44a3d004a3b7c6e0f2a7dbb18275
farhanmn <najibfarhan3@gmail.com>
2017-11-07 05:02:35 +0700

    Korespondensi footer

--- resources/views/facilities-sec.blade.php
+++ resources/views/facilities-sec.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app')
+@extends('layout.app-with-footer')
 
 @section('title','Fasilitas Penunjang | IPC Car Terminal')
 
@@ -95,45 +95,4 @@
         </div>
     </section>
     <!---->
-    <!--footer-->
-    <!-- <div class="hiddenSec"></div> -->
-    <footer class="footer-1">
-    <section id="contact" class="wow fadeInLeft">
-    <section class="row noMag">
-      <div class="col-md-4">
-        <section class="contactInfo">
-          <h4 class="roboBold">Kontak Kami</h4>
-          <section class="row mg-lr-15 mg-t-30">
-            <i class="fa fa-map-marker fa-2x or floatLeft mg-r-20"></i>
-            <p class="small">JL Sindang Laut, Cilincing Jakarta Utara, 14110</p>
-          </section>
-          <section class="row mg-lr-15 mg-t-10">
-            <i class="fa fa-envelope-o fa-lg or floatLeft mg-r-20"></i>
-            <p class="small">info@indonesiacarterminal.co.id</p>
-          </section>
-          <section class="row mg-lr-15 mg-t-10">
-            <i class="fa fa-phone fa-lg or floatLeft mg-r-20"></i>
-            <p class="small">+62 21 4393 2251</p>
-          </section>
-          <section class="row">
-            <div class="socialMedia text-center">
-              <ul class="noPad">
-                <li><a class="fb" href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a class="tw" href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a class="ig" href="#"><i class="fa fa-instagram"></i></a></li>
-              </ul>
-            </div>
-          </section>
-        </section>
-      </div>
-      <div class="col-md-8 noPad hidden-400">
-        <div class="map">
-          <div class="overlay-map"></div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.1833851621095!2d106.91224451476846!3d-6.105991995578615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe329d8258290e493!2sTanjung+Priok+Car+Terminal+(TPT)!5e0!3m2!1sen!2sid!4v1501491377262" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-      </div>
-    </section>
-  </section>
-    </footer>
-    <!--/footer-->
 @endsection
