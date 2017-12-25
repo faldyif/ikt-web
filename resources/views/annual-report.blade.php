46e4bfa2e0b65b41bb1c62602451b30665fe6467
farhanmn <najibfarhan3@gmail.com>
2017-12-25 22:35:58 +0700

    Annual report

--- resources/views/annual-report.blade.php
+++ resources/views/annual-report.blade.php
@@ -1,4 +1,12 @@
 @extends('layout.app-with-footer')
+<style type="text/css">
+  .annual{
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  .annual:hover{
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.5);
+  }
+</style>
 
 @section('title','Annual Report | IPC Car Terminal')
 
@@ -18,11 +26,50 @@
     <section>
       <div class="container-fluid wow fadeInUpBig">
         <section class="row pd-t-20">
-          <div class="col-md-8 col-sm-10 col-sm-offset-1 pd-bt-30">
+          <div class="col-md-10 col-sm-10 col-sm-offset-1 pd-bt-30">
             <div class="col-md-12">
             <hr class="hrSpec hrSpecOrange">
             <h3 class="mg-b-30 roboBold">Annual Report</h3>
-              <h2>Sedang menunggu konten...</h2>
+              <div class="row">
+                <div class="col-md-12 annual pd-bt-20 mg-bt-10 flex-row-center">
+                  <div class="col-md-3">
+                    <!-- foto -->
+                    <img src="{{ url('img/thumb.jpg') }}" class="img-responsive" style="width: 150px; height: auto;">
+                  </div>
+                  <div class="col-md-9">
+                    <!-- konten -->
+                    <div class="col-md-6">
+                    <p>Judul: Annual Report PT Indonesia Kendaraan Terminal 2016</p>
+                    <p>Halaman: 70 Halaman</p>
+                    <p>Tahun: 2016</p>
+                    </div>
+                    <div class="col-md-6">
+                      <p>Deskripsi: </p>
+                      <a href="#">Download</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 annual pd-bt-20 mg-bt-10 flex-row-center">
+                  <div class="col-md-3">
+                    <!-- foto -->
+                    <img src="{{ url('img/thumb.jpg') }}" class="img-responsive" style="width: 150px; height: auto;">
+                  </div>
+                  <div class="col-md-9">
+                    <!-- konten -->
+                    <div class="col-md-6">
+                    <p>Judul: Annual Report PT Indonesia Kendaraan Terminal 2017</p>
+                    <p>Halaman: 80 Halaman</p>
+                    <p>Tahun: 2017</p>
+                    </div>
+                    <div class="col-md-6">
+                    <p>Deskripsi: </p>
+                    <a href="#">Download</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
